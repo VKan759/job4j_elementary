344caf7aa4a16b0acad395c2cb2513734e4af223
Vyacheslav Kan <vkan745@gmail.com>
2024-01-27 22:26:34 +0600

    4.2. Тернарное сравнение [#189 #476686] (добавлены тесты)

--- src/test/java/ru/job4j/condition/MaxTest.java
+++ src/test/java/ru/job4j/condition/MaxTest.java
@@ -6,11 +6,29 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 class MaxTest {
     @Test
-    void whenMax1To2Then2() {
+    void whenMax2To1Then2() {
         int left = 1;
         int right = 2;
         int result = Max.max(left, right);
         int expected = 2;
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    void whenMax1To2Then1() {
+        int left = 2;
+        int right = 1;
+        int result = Max.max(left, right);
+        int expected = 2;
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    void when1IsEqual2Then1() {
+        int left = 3;
+        int right = 3;
+        int result = Max.max(left, right);
+        int expected = 3;
         assertThat(result).isEqualTo(expected);
     }
 }
