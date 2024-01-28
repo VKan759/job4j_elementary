1991367a8f1cf375a26549727a154e17643292e0
Vyacheslav Kan <vkan745@gmail.com>
2024-01-28 18:15:27 +0600

    4.3.5. Множественное логическое выражение И [#9461]

---
+++ src/main/java/ru/job4j/condition/ChessBoard.java
@@ -0,0 +1,21 @@
+package ru.job4j.condition;
+
+public class ChessBoard {
+    public static int way(int x1, int y1, int x2, int y2) {
+        int result = 0;
+        if (isValid(x1) && isValid(x2)
+                && isValid(y1) && isValid(y2)) {
+            if (x1 == y1 && x2 == y2
+                    || (x1 == Math.abs(y1 - 7)) && (x2 == Math.abs(y2 - 7))
+                    || (x1 + 1 == y1 && x2 + 1 == y2)
+            ) {
+                result = Math.abs(x2 - x1);
+            }
+        }
+        return result;
+    }
+
+    private static boolean isValid(int coordinate) {
+        return coordinate >= 0 && coordinate <= 7;
+    }
+}
