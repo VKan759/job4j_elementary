b012a0f7da3fda632e8367ae620086c734f35a34
Vyacheslav Kan <vkan745@gmail.com>
2024-01-29 19:26:28 +0600

    4.5. Отладка программы в IDEA [#159464] (убрал смешение || && и лишние условия)

--- src/main/java/ru/job4j/condition/ThreeMax.java
+++ src/main/java/ru/job4j/condition/ThreeMax.java
@@ -3,12 +3,11 @@ package ru.job4j.condition;
 public class ThreeMax {
     public static int max(int first, int second, int third) {
         int result = first;
-        if (first <= second && second > third
-                || first < second && second >= third) {
+        if (first <= second && second > third) {
             result = second;
-        }
-        if (third >= first && second < third
-                || third > first && second <= third) {
+        } else if (first < second && second >= third) {
+            result = second;
+        } else if (third >= first && second < third) {
             result = third;
         }
         return result;
