c9b9c180258f55adb5486e33e3ecc6e3ca05f96d
Vyacheslav Kan <vkan745@gmail.com>
2024-01-30 13:38:19 +0600

    5.1. Подсчет суммы чётных чисел в диапазоне [#192 #477504] (cокращено sum +=i)

--- src/main/java/ru/job4j/loop/Counter.java
+++ src/main/java/ru/job4j/loop/Counter.java
@@ -25,7 +25,7 @@ public class Counter {
         System.out.println(sum(1, 1));
 
         System.out.println(sumByEven(0, 10));
-        System.out.println(sumByEven(3, 8));
+        System.out.println(sumByEven(3, 9));
         System.out.println(sumByEven(1, 1));
     }
 }
