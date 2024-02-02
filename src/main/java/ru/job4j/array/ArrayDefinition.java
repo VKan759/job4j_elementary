c7dd5689d4b8def8d43e18c9e60d088c782b0283
Vyacheslav Kan <vkan745@gmail.com>
2024-02-02 12:46:58 +0600

    6.0.3. Заполнение массива. [#226300]

--- src/main/java/ru/job4j/array/ArrayDefinition.java
+++ src/main/java/ru/job4j/array/ArrayDefinition.java
@@ -9,5 +9,15 @@ public class ArrayDefinition {
         System.out.println("Размер массива ages равен: " + ages.length);
         System.out.println("Размер массива surnames равен: " + surnames.length);
         System.out.println("Размер массива prices равен: " + prices.length);
+
+        String[] names = new String[4];
+        names[0] = "Vasya";
+        names[1] = "Dima";
+        names[2] = "Denis";
+        names[3] = "Slava";
+
+        for (int i = 0; i < names.length; i++) {
+            System.out.println(names[i]);
+        }
     }
 }
