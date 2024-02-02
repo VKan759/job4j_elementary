d014c16fd60cdac841c40641236e1c12b4193a48
Vyacheslav Kan <vkan745@gmail.com>
2024-02-02 12:49:36 +0600

    6.0.3. Заполнение массива. [#226300]

--- src/main/java/ru/job4j/array/ArrayDefinition.java
+++ src/main/java/ru/job4j/array/ArrayDefinition.java
@@ -15,9 +15,9 @@ public class ArrayDefinition {
         names[1] = "Dima";
         names[2] = "Denis";
         names[3] = "Slava";
-
-        for (int i = 0; i < names.length; i++) {
-            System.out.println(names[i]);
-        }
+        System.out.println(names[0]);
+        System.out.println(names[1]);
+        System.out.println(names[2]);
+        System.out.println(names[3]);
     }
 }
