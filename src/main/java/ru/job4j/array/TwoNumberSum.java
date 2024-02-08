a14a524d204cc7eecfe8d90836780cc7756c8880
Vyacheslav Kan <vkan745@gmail.com>
2024-02-08 10:58:40 +0600

    6.8. Метод двух указателей [#225 #479618] (сделана непрерывная цепочка if-else)

--- src/main/java/ru/job4j/array/TwoNumberSum.java
+++ src/main/java/ru/job4j/array/TwoNumberSum.java
@@ -6,21 +6,15 @@ public class TwoNumberSum {
         int i = 0;
         int j = array.length - 1;
         int[] newArray = new int[0];
-        while (i < array.length) {
-            if (array.length >= 2) {
-                if (array[i] + array[j] == target) {
-                    newArray = new int[]{i, j};
-                    break;
-                }
-                if (array[i] + array[j] > target) {
-                    j--;
-                    if (array[i] + array[j] == target) {
-                        newArray = new int[]{i, j};
-                        break;
-                    }
-                }
+        while (i < array.length && array.length >= 2) {
+            if (array[i] + array[j] == target) {
+                newArray = new int[]{i, j};
+                break;
+            } else if (array[i] + array[j] > target) {
+                j--;
+            } else {
+                i++;
             }
-            i++;
         }
         return newArray;
     }
