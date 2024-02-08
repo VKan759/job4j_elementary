928f62bfb16157d539f8277e32f790eeb3e15974
Vyacheslav Kan <vkan745@gmail.com>
2024-02-08 11:33:03 +0600

    6.8.1. Жадный алгоритм. Сдача в кофе машине [#34741 #479649] (сделана цепочка if-else, удалена переменная change, удалены пробелы)

--- src/main/java/ru/job4j/array/Machine.java
+++ src/main/java/ru/job4j/array/Machine.java
@@ -3,14 +3,12 @@ package ru.job4j.array;
 import java.util.Arrays;
 
 public class Machine {
-    @SuppressWarnings("checkstyle:EmptyForIteratorPad")
     public static int[] change(int money, int price) {
         int[] coins = {10, 5, 2, 1};
         int[] result = new int[100];
         int size = 0;
         money = money - price;
         int moneyIndex = 0;
-
         while (money > 0) {
             if (money >= coins[moneyIndex]) {
                 money = money - coins[moneyIndex];
